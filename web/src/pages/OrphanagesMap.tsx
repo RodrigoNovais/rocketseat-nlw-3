8933bcea9e7b756254cf2dd4df74c311d225c2f8
Rodrigo Tolentino de Novais <roo.novais@gmail.com>
2020-10-12 20:52:16 -0300

    📦 feat(web): Add mapbox map with react-leaflet

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom'
 
 import { FiPlus } from 'react-icons/fi'
 
+import { Map, TileLayer } from 'react-leaflet'
+
 import mapMarkerImg from '../images/map-marker.svg'
 
+import 'leaflet/dist/leaflet.css'
 import '../styles/pages/orphanages-map.css'
 
 const OrphanagesMap = () => {
@@ -24,11 +27,13 @@ const OrphanagesMap = () => {
                 </footer>
             </aside>
 
+            <Map center={[-23.7231993,-46.5799926]} zoom={15} style={{ width: '100%', height: '100%', zIndex: 0 }}>
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+            </Map>
+
             <Link to='' className='create-orphanage'>
                 <FiPlus size={32} color='#FFFFFF' />
             </Link>
-
-            <div></div>
         </div>
     )
 }
