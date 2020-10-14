5bf91e5aa6a8161770340479206261bdb62c5ef8
Rodrigo Tolentino de Novais <roo.novais@gmail.com>
2020-10-14 17:04:08 -0300

    📦 feat(server): Input validation middleware

--- server/src/routes/orphanages.ts
+++ server/src/routes/orphanages.ts
@@ -4,18 +4,31 @@ import multer from '@koa/multer'
 import { multerOptions } from '../config'
 
 import orphanagesController from '../controllers/orphanages.controller'
+import orphanageValidator from '../validators/orphanage.validator'
+import validationMiddleware from '../middlewares/validation.middleware'
 
 const routes = new Router({ prefix: '/orphanages' })
 const upload = multer(multerOptions)
 
-routes.get('/', orphanagesController.index)
-routes.get('/:id', orphanagesController.show)
+routes.get('/',
+    validationMiddleware(orphanageValidator.index),
+    orphanagesController.index)
+
+routes.get('/:id',
+    validationMiddleware(orphanageValidator.show),
+    orphanagesController.show)
 
 routes.post('/',
     upload.array('images'),
+    validationMiddleware(orphanageValidator.store),
     orphanagesController.store)
 
-routes.put('/:id',orphanagesController.update)
-routes.del('/:id', orphanagesController.destroy)
+routes.put('/:id',
+    validationMiddleware(orphanageValidator.update),
+    orphanagesController.update)
+
+routes.del('/:id',
+    validationMiddleware(orphanageValidator.destroy),
+    orphanagesController.destroy)
 
 export default routes
