2a5873ed4385b9099f93244e354d7eb6109ebf45
Rodrigo Tolentino de Novais <roo.novais@gmail.com>
2020-10-21 07:57:58 -0300

    📦 feat(mobile): Private Routes

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack'
+
+import CreateOrphanage from '../pages/CreateOrphanage'
+
+import OrphanagesDetails from '../pages/OrphanageDetails'
+import OrphanagesMap from '../pages/OrphanagesMap'
+
+import Header from '../components/Header'
+
+const { Navigator, Screen } = createStackNavigator()
+
+const Routes: React.FC = () => {
+    return (
+        <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#F2F3F5' } }}>
+            <Screen name='OrphanagesMap' component={OrphanagesMap} />
+            <Screen name='OrphanageDetails' component={OrphanagesDetails} options={{
+                headerShown: true,
+                header: () => <Header title='Orfanato' showCancel={false} />
+            }} />
+
+            <Screen name='CreateOrphanage' component={CreateOrphanage} />
+        </Navigator>
+    )
+}
+
+export default Routes
